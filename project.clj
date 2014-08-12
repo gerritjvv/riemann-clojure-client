1f9fddb4eb216b7fb2020a83dda74cecd9e23f13
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2014-08-12 23:54:51 +0200

    add nt as groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nt/riemann-java-client-proto2.4.1 "0.2.12-SNAPSHOT"
+(defproject nt/riemann-clojure-client-proto2.4.1 "0.2.12-SNAPSHOT"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
